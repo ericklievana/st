f65432c26ffd5a4f8df764805fbe503953c741ef
Erick Lievana Poy <ericklievana@gmail.com>
2023-11-19 17:37:28 -0600

    Add w3m patch
    https://st.suckless.org/patches/w3m/

--- x.c
+++ x.c
@@ -1800,6 +1800,8 @@ xpushtitle(void)
 int
 xstartdraw(void)
 {
+	if (IS_SET(MODE_VISIBLE))
+		XCopyArea(xw.dpy, xw.win, xw.buf, dc.gc, 0, 0, win.w, win.h, 0, 0);
 	return IS_SET(MODE_VISIBLE);
 }
 
